67687f2cc75edfc1014c28373c68e02bc2594d0b
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2021-05-25 16:22:00 -0400

    reviews
    added reviews, a few custom buttons, fixed some formatting stuff, and absolutely did not properly implement a gallery

---
+++ buttons.js
@@ -0,0 +1,9 @@
+function loadAllReviews()
+{
+    window.location = "https://www.google.com/search?hl=en-US&gl=us&q=The+Village+Diner,+299+Keswick+Ave,+Glenside,+PA+19038&ludocid=13376675094642473713&lsig=AB86z5UuESbxiDdwWLwJ_xMow8WX#lrd=0x89c6b09b4289ec5d:0xb9a37f203bfabaf1,1";
+}
+
+function writeAReview()
+{
+    window.location = "https://www.google.com/search?hl=en-US&gl=us&q=The+Village+Diner,+299+Keswick+Ave,+Glenside,+PA+19038&ludocid=13376675094642473713&lsig=AB86z5UuESbxiDdwWLwJ_xMow8WX#lrd=0x89c6b09b4289ec5d:0xb9a37f203bfabaf1,3";
+}
