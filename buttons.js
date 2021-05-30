ae5bac8215451af18cc6c1b39bc22cfe33d44b18
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2021-05-30 01:57:37 -0400

    gallery and other stuffs

--- buttons.js
+++ buttons.js
@@ -7,3 +7,8 @@ function writeAReview()
 {
     window.location = "https://www.google.com/search?hl=en-US&gl=us&q=The+Village+Diner,+299+Keswick+Ave,+Glenside,+PA+19038&ludocid=13376675094642473713&lsig=AB86z5UuESbxiDdwWLwJ_xMow8WX#lrd=0x89c6b09b4289ec5d:0xb9a37f203bfabaf1,3";
 }
+
+function enlargeMenu()
+{
+    window.location = "https://www.musthavemenus.com/menu/viewMenu.do?token=bd3786aa-c805-4522-a186-1d00f623eeaa&lite=true";
+}
