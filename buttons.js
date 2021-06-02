01721a50724cc4a8a7ac5027f1f8efbf1418644d
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2021-06-02 03:07:57 -0400

    update stuffs

--- buttons.js
+++ buttons.js
@@ -12,3 +12,8 @@ function enlargeMenu()
 {
     window.location = "https://www.musthavemenus.com/menu/viewMenu.do?token=bd3786aa-c805-4522-a186-1d00f623eeaa&lite=true";
 }
+
+function loadAllUpdates()
+{
+    window.location = "https://www.google.com/search?q=village+diner&rlz=1C1PRFI_enUS798US798&oq=vill&aqs=chrome.0.69i59j69i57j69i59j46i433l2j69i60l3.2486j0j7&sourceid=chrome&ie=UTF-8#lpstate=pid:6563961721082804469";
+}
