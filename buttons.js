6ec435ac76cef98ea26d237a83e0ef1d13ca36e1
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2021-06-02 15:24:48 -0400

    More updates, contact us, etc

--- buttons.js
+++ buttons.js
@@ -17,3 +17,8 @@ function loadAllUpdates()
 {
     window.location = "https://www.google.com/search?q=village+diner&rlz=1C1PRFI_enUS798US798&oq=vill&aqs=chrome.0.69i59j69i57j69i59j46i433l2j69i60l3.2486j0j7&sourceid=chrome&ie=UTF-8#lpstate=pid:6563961721082804469";
 }
+
+function getDirections()
+{
+    window.location = "https://www.google.com/maps/dir//The+Village+Diner/@40.1064539,-75.2175066,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c6b09b4289ec5d:0xb9a37f203bfabaf1!2m2!1d-75.1474699!2d40.1064721";
+}
